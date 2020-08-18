fca1be07aedf529e7519eafb0c33449e3ff8ea60
jlandercy <jeanlandercy@live.com>
2020-08-18 16:19:10 +0200

    Bare KMI is working

--- odapi/connectors/opendata/irmkmi.py
+++ odapi/connectors/opendata/irmkmi.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import json
 
 import numpy as np
 import pandas as pd
@@ -25,70 +26,61 @@ class IRMKMI(TimeSeriesAPI):
         """Fetch method"""
         rep = requests.get(self.endpoint[endpoint].format(**params or {}))
         settings.logger.debug("{} [{}]: {}".format(rep.request.method, rep.status_code, rep.url))
+        # Normalize bytes:
+        data = rep.json()
+        for rec in data["features"]:
+            rec["properties"]["qc_flags"] = json.loads(rec["properties"]["qc_flags"])
+
         if mode == 'frame':
-            return pd.json_normalize(rep.json())
+            return pd.json_normalize(data["features"])
         elif mode == 'json':
-            return rep.json()
+            return data
         else:
             return rep.content
+
+    def normalize(self, df):
+        df = df.filter(regex='geometry\.|properties\.')
+        df = df.melt(id_vars=["properties.code", "properties.timestamp", "geometry.coordinates"])
+        df["measurekey"] = df["variable"].apply(lambda x: x.split('.')[-1])
+        coords = df.pop("geometry.coordinates")
+        df["site_lon"] = coords.apply(lambda x: x[0])
+        df["site_lat"] = coords.apply(lambda x: x[1])
+        df = df.rename(columns={"properties.code": "siteid",  "properties.timestamp": "timestamp"})
+        df = df[df["measurekey"] != "bbox"]
+        df = df[df["measurekey"] != "type"]
+        df.pop("variable")
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
+        return df
 
     def get_metadata(self, **kwargs):
         """Get Metadata"""
         # Download Timeserie Metadata:
         df = self.fetch('metadata')
+        df = self.normalize(df)
+        df = df.groupby(["siteid", "measurekey"]).agg({"site_lon": "first", "site_lat": "first"})
+        df = df.reset_index()
         settings.logger.debug("FRAME: {} metadata fetched".format(df.shape))
         return df
 
-    def get_records(self, identifiers, start=None, stop=None, span=None, sentinel=-99.9):
+    def get_records(self, identifiers, start=None, stop=None, span=None):
         """Get Records"""
         # Irceline API select timestamp on end of sample period and include both sides (1 year at once max)
         params = self.prepare_parameters(identifiers, start=start, stop=stop, span=span, timezone='UTC')
-        return
-
-    @property
-    def sites(self):
-        df = self.meta.groupby(['siteid', 'sitekey', 'sitename', 'lon', 'lat']).agg({
-            'serieid': list, 'measureid': set, 'measurekey': set,
-            'started': 'min', 'stopped': 'max'
-        })
-        df['count'] = df['serieid'].apply(len)
-        df = df.sort_values('sitekey').reset_index()
-        df = df.merge(self.table('sitetypes'), how='left')
+        df = self.fetch('records', params=params)
+        df = self.normalize(df).drop(["site_lon", "site_lat"], axis=1)
         return df
 
-    @property
-    def measures(self):
-        df = self.meta.groupby(['measureid', 'measurekey', 'measurename']).agg({
-            'serieid': list, 'siteid': set, 'sitekey': set, 'serieunits': set,
-            'started': 'min', 'stopped': 'max'
-        })
-        df['count'] = df['serieid'].apply(len)
-        df = df.sort_values('measurekey').reset_index()
-        return df.merge(self.table('factors'), how='left')
-
-    @property
-    def events(self):
-        df = pd.DataFrame(self.tables['events'])
-        df['start'] = pd.to_datetime(df['start'], utc=True)
-        df['stop'] = pd.to_datetime(df['stop'], utc=True)
-        df['start'] = df['start'].fillna(pd.Timestamp.min.tz_localize('UTC'))
-        df['stop'] = df['stop'].fillna(pd.Timestamp.max.tz_localize('UTC'))
-        return df
-
-
-"""
-https://opendata.meteo.be/service/ows?service=WFS&version=2.0.0&request=GetFeature&typenames=aws:aws_10min&outputformat=json&CQL_FILTER=((BBOX(the_geom,3.201846,50.193663,5.255236,51.347375,%20%27EPSG:4326%27))%20AND%20(timestamp%20%3E=%20%272020-08-01%2005:00:00%27%20AND%20timestamp%20%3C=%20%272020-08-14%2005:00:00%27))&sortby=timestamp
-"""
 
 def main():
     pd.options.display.max_columns = 20
     pd.options.display.max_rows = 30
 
     c = IRMKMI()
-    s = c.select(measurekey=['NO', 'CO2'], sitekey='41.*')
-    print(s)
+    m = c.get_metadata()
+    print(m)
 
-    print(c.sites)
+    r = c.get_records(None)
+    print(r)
 
     sys.exit(0)
 

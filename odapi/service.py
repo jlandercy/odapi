02a2cf81b7c62b4e3a0fc4e977de92e91703095c
jeanlandercy <jeanlandercy@live.com>
2022-11-10 11:31:03 +0100

    New Scheduler does not accept pandas timestamp to feed the process

--- odapi/service.py
+++ odapi/service.py
@@ -41,7 +41,7 @@ def main():
     clargs = argparse.ArgumentParser(description='Irceline Service')
     clargs.add_argument('--program', type=str, default=str(settings.user / 'program.json'),
                         help='Program definition path (eg.: ~/odapi/program.json)')
-    clargs.add_argument('--verbose', type=int, default=10, help='Verbose Level')
+    clargs.add_argument('--verbose', type=int, default=40, help='Verbose Level')
     args = clargs.parse_args()
 
     # Set Log Level
